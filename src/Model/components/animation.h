a960fe62c765a28b7e163c4d011c744fedf1992f
CrazySpottedDove <3230104178@zju.edu.cn>
2025-07-01 16:14:16 +0800

    change animation

--- src/Model/components/animation.h
+++ src/Model/components/animation.h
@@ -15,6 +15,7 @@ struct Animation
     State       state;
     double      rotation = 0.0;    // 旋转角度
     bool        pending  = true;   // 动画正在进行
-    std::size_t frame_id = 0;      // 动画进行到第几帧
-    std::string prefix;            // 动画的前缀名
+    bool        flip     = false;
+    std::size_t frame_id = 0;   // 动画进行到第几帧
+    std::string prefix;         // 动画的前缀名
 };

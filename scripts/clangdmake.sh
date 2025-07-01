a34f3e60050bec6e8d30c39e20565f2a8ffb805f
CrazySpottedDove <3230104178@zju.edu.cn>
2025-07-01 17:06:13 +0800

    modify build script

--- scripts/clangdmake.sh
+++ scripts/clangdmake.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cmake "$1" -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B build
+cmake "$1" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B build
